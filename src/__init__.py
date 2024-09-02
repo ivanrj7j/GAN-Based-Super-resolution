a89312e94e1eb6644834464565ffa7e7114ee1cc
ivanrj7j <ivanrj7j@gmail.com>
2024-09-02 17:53:41 +0530

    implemented vggloss

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
 from src.generator import Generator
 from src.discriminator import Discriminator
+from src.loss import VGGLoss
---
+++ src/loss.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+from torchvision.models import vgg19
+from torchvision.models.vgg import VGG19_Weights
+import config
+
+class VGGLoss(nn.Module):
+    """
+    VGG loss using pretrained vgg19 model
+    """
+    def __init__(self, device="cuda", *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.vgg = vgg19(weights=VGG19_Weights.DEFAULT).features[:36].eval().to(device)
+        self.loss = nn.MSELoss()
+
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+
+    def forward(self, yhat, y):
+        return self.loss.forward(self.vgg(yhat), self.vgg(y))
